703f172dc9c11be3fc4db7557db33d4bbabdc302
UjjwalChoudhary2001 <93510739+UjjwalChoudhary2001@users.noreply.github.com>
2024-07-31 11:48:08 +0000

    Pattern 3

---
+++ Patterns/3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*Pattern-2
+1
+12
+123
+1234
+*/
+
+int main() {
+  int n;
+  cout << "Enter the number of rows" << endl;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << j + 1 << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
