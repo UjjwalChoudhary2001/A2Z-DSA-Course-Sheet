6d8e1cd268f5ba4caeaa36b153fcb16df39cbe97
UjjwalChoudhary2001 <93510739+UjjwalChoudhary2001@users.noreply.github.com>
2024-08-27 06:47:11 +0000

    Pattern-8

--- .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
+++ .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
@@ -15,9 +15,30 @@ void pattern6(int n) {
       cout << " ";
     }
 
-    cout<<endl;
+    cout << endl;
   }
-  
+}
+void pattern7(int n) {
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+
+    for (int j = 0; j < 2 * n - (2*i+1); j++) {
+      cout << "*";
+    }
+
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+
+    cout << endl;
+  }
+}
+void pattern8(int n){
+  pattern6(n);
+  pattern7(n);
 }
 
 
@@ -25,6 +46,6 @@ int main() {
   int n;
   cout << "Enter the number of rows" << endl;
   cin >> n;
-  pattern6(n);
+  pattern8(n);
   return 0;
 }
