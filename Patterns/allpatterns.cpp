fa4cf263d29faf18333c30162f81c03eea8479e2
UjjwalChoudhary2001 <93510739+UjjwalChoudhary2001@users.noreply.github.com>
2024-08-27 07:40:00 +0000

    Pattern-10

--- Patterns/allpatterns.cpp
+++ Patterns/allpatterns.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void pattern6(int n) {
+void pattern2(int n){
+  for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<=i;j++)
+        {
+          cout<<"* ";
+        }
+      cout<<endl;
+    }
+}
+void pattern3(int n){
+  for(int i=n-1;i>=0;i--)
+    {
+      for(int j=i+1;j>0;j--)
+        {
+          cout<<"* ";
+        }
+      cout<<endl;
+    }
+  
+}
+void pattern7(int n) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       cout << " ";
@@ -18,7 +39,7 @@ void pattern6(int n) {
     cout << endl;
   }
 }
-void pattern7(int n) {
+void pattern8(int n) {
   for (int i = 0; i < n; i++) {
 
     for (int j = 0; j < i; j++) {
@@ -36,16 +57,39 @@ void pattern7(int n) {
     cout << endl;
   }
 }
-void pattern8(int n){
-  pattern6(n);
+void pattern9(int n){
   pattern7(n);
+  pattern8(n);
 }
+void pattern10(int n)
+{
+  for(int i=0;i<2*n-1;i++)
+    {
+      if(i<=n-1)
+      {
+        int stars = i+1;
+        for(int j=0;j<stars;j++)
+          {
+            cout<<"* ";
+          }
+      }
+      else
+      {
+        int stars = 2*n-i-1;
+        for(int j=0;j<stars;j++)
+          {
+            cout<<"* ";
+          }
+      }
 
+      cout<<endl;
+    }
+}
 
 int main() {
   int n;
   cout << "Enter the number of rows" << endl;
   cin >> n;
-  pattern8(n);
+  pattern10(n);
   return 0;
 }
