2d131468c43935ba505453119ea29de25538cbc0
UjjwalChoudhary2001 <93510739+UjjwalChoudhary2001@users.noreply.github.com>
2024-08-27 09:00:18 +0000

    Pattern

--- .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
+++ .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
@@ -61,8 +61,7 @@ void pattern9(int n){
   pattern7(n);
   pattern8(n);
 }
-void pattern10(int n)
-{
+void pattern10(int n){
   for(int i=0;i<2*n-1;i++)
     {
       if(i<=n-1)
@@ -85,11 +84,53 @@ void pattern10(int n)
       cout<<endl;
     }
 }
+void pattern11(int n){
+  for(int i=0;i<n;i++)
+    {
+      int start=-1;
+      if(i%2==0)
+      {
+        start=1;
+      }
+      else{
+        start=0;
+      }
 
+      for(int j=0;j<=i;j++)
+        {
+          cout<<start<<" ";
+          start=1-start;
+        }
+      cout<<endl;
+    }
+}
+void pattern(int n){
+  //This patterns is like pattern-12 with stars instead of numbers
+  for(int i=0;i<n;i++)
+    {
+      //stars
+      for(int j=0;j<i;j++)
+        {
+          cout<<"*";
+        }
+      //spaces
+      for(int j=0;j<2*n-2-2*i;j++)
+        {
+          cout<<" ";
+        }
+      //stars
+      for(int j=0;j<i;j++)
+        {
+          cout<<"*";
+        }
+
+      cout<<endl;
+    }
+}
 int main() {
   int n;
   cout << "Enter the number of rows" << endl;
   cin >> n;
-  pattern10(n);
+  pattern(n);
   return 0;
 }
