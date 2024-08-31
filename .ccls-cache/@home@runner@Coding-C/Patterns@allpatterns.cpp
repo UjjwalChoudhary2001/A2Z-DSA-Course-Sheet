e3e607f9319ed6daed1673a9844b8ad31f6d2b82
UjjwalChoudhary2001 <93510739+UjjwalChoudhary2001@users.noreply.github.com>
2024-08-31 07:20:11 +0000

    Pattern-13

--- .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
+++ .ccls-cache/@home@runner@Coding-C/Patterns@allpatterns.cpp
@@ -148,10 +148,25 @@ void pattern12(int n){
       cout<<endl;
     }
 }
+void pattern13(int n){
+  int k=1;
+  for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<=i;j++)
+        {
+          cout<<k<<" ";
+          k++;
+        }
+      cout<<endl;
+    }
+}
+
+
+
 int main() {
   int n;
   cout << "Enter the number of rows" << endl;
   cin >> n;
-  pattern12(n);
+  pattern13(n);
   return 0;
 }
